54be45e7fcccf7eb0295bb5d51fc08d9ef7486df
Admin <Admin@DESKTOP-79JJA0I>
2023-04-04 09:10:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Traffic_lighters;
+using static Traffic_lighters.TrafficLighterModes;
+
+Console.WriteLine($"Application start {DateTime.Now}");
+
+TrafficLighterModes mode = new ();
+
+mode.CheckStatus();
+
+await mode.Work_Mode(WorkMode.First);
+
+
